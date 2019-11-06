7b0bf34b43afe5524680c053963ecb4ccab9b2a1
Moonlightwsn <543456277@qq.com>
2019-11-06 21:17:19 +0800

    更新mui-card-header

--- src/card-header/card-header.js
+++ src/card-header/card-header.js
@@ -1,7 +1,16 @@
 Component({
   properties: {
     avatar: String,
+    avatarText: String,
+    avatarSrc: String,
+    avatarColor: String,
+    avatarBackgroundColor: String,
     action: String,
+    actionColor: {
+      type: String,
+      value: 'rgba(0, 0, 0, 0.54)',
+    },
+    actionBackgroundColor: String,
     title: String,
     subheader: String,
   },
