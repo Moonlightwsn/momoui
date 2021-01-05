b20287aa03c305b423a0444d95f9d017f4e67919
Moonlightwsn <543456277@qq.com>
2021-01-06 00:03:11 +0800

    update appBar examples page

--- tools/demo/pages/app-bar/app-bar.js
+++ tools/demo/pages/app-bar/app-bar.js
@@ -6,12 +6,16 @@ Page({
     openMenu: false,
     openMenu2: false,
     openMenu3: false,
+    openMenu4: false,
+    auth: true,
   },
   onLoad() {
     this.setData({
       CloseMenu: this.CloseMenu.bind(this),
       CloseMenu2: this.CloseMenu2.bind(this),
       CloseMenu3: this.CloseMenu3.bind(this),
+      CloseMenu4: this.CloseMenu4.bind(this),
+      changeAuth: this.changeAuth.bind(this),
     })
   },
   OpenMenu() {
@@ -31,5 +35,14 @@ Page({
   },
   CloseMenu3() {
     this.setData({openMenu3: false})
+  },
+  OpenMenu4() {
+    this.setData({openMenu4: true})
+  },
+  CloseMenu4() {
+    this.setData({openMenu4: false})
+  },
+  changeAuth() {
+    this.setData({auth: !this.data.auth})
   },
 })
